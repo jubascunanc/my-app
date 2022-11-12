c75abc3cfca153f198859ac0a5bfbca12482c6d4
Juan Jose Bascuñan <jubascunanc@udd.cl>
2022-11-12 10:22:39 -0400

    push repo

---
+++ src/componentes/CartWidget/ItemListContainer/ItemList.jsx
@@ -0,0 +1,13 @@
+import '../ItemList/ItemList.css'
+import Item from '../Item/Item'
+
+const ItemList = ({products, setPage }) => {
+    return(
+            
+        <div className='ListGroup'>
+            {products.map(prod => <Item key={prod.id} {...prod} setPage={setPage}/>)}
+        </div>    
+    )
+}
+
+export default ItemList
